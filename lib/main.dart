06b01fd059d9e2bc174e489a26c37eb4bb5303dc
znixonz <znixonz@yahoo.com>
2020-05-03 01:51:41 +0800

    counter & header

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:covid19/constant.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,53 +11,104 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Covid 19',
       theme: ThemeData(
-        scaffoldBackgroundColor: kBackgroundColor,
-        fontFamily: "Poppins",
-        textTheme: TextTheme(
-          body1: TextStyle(color:kBodyTextColor),
-        )
-      ),
+          scaffoldBackgroundColor: kBackgroundColor,
+          fontFamily: "Poppins",
+          textTheme: TextTheme(
+            body1: TextStyle(color: kBodyTextColor),
+          )),
       home: HomeScreen(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final controller = ScrollController();
+  double offset = 0;
+
+  @override
+  void inistate() {
+    super.initState();
+    controller.addListener(onScroll);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  void onScroll() {
+    setState(() {
+      offset = (controller.hasClients) ? controller.offset : 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-        ClipPath(
-          clipper: MyClipper(),
-                  child: Container(
-          height: 350,
-          width: double.infinity,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topRight,
-              end: Alignment.bottomLeft,
-              colors:[
-                Color(0xFF3383CD),
-                Color(0xFF11249F),
+      body: SingleChildScrollView(
+        controller: controller,
+        child: Column(children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(left: 40, top: 50, right: 20),
+            height: 350,
+            width: double.infinity,
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    Color(0xFF3383CD),
+                    Color(0xFF11249F),
+                  ],
+                ),
+                image: DecorationImage(
+                  image: AssetImage("assets/images/virus.png"),
+                )),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Align(
+                    alignment: Alignment.topRight,
+                    child: SvgPicture.asset("assets/icons/menu.svg")),
+                SizedBox(height: 20),
+                Expanded(
+                    child: Stack(
+                  children: <Widget>[
+                    SvgPicture.asset(
+                      "assets/icons/Drcorona.svg",
+                      width: 230,
+                      fit: BoxFit.fitWidth,
+                      alignment: Alignment.topCenter,
+                    )
+                  ],
+                ))
               ],
-              ),
-              image: DecorationImage(image: AssetImage("assets/images/virus.png"),)
-          ),
-          ),
-        )]
+            ),
+          )
+        ]),
       ),
-      
     );
   }
 }
 
-class MyClipper extends CustomClipper<Path>{
+// class HomeScreen extends StatelessWidget {
+
+// }
+
+class MyClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     var path = Path();
-    path.lineTo(0, size.height - 80 );
-    path.quadraticBezierTo(size.width/2, size.height, size.width, size.height - 80);
+    path.lineTo(0, size.height - 80);
+    path.quadraticBezierTo(
+        size.width / 2, size.height, size.width, size.height - 80);
     path.lineTo(size.width, 0);
     path.close();
     return path;
